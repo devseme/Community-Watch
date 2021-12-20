5569c31b4e7a221613e13e74b2f0b7b9403cb7e7
devseme <ian.ochenge@student.moringaschool.com>
2021-12-20 08:03:24 +0300

    added templates folder

--- nyumbani/settings.py
+++ nyumbani/settings.py
@@ -10,7 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
+import cloudinary
+import django_heroku
+import dj_database_url
 from pathlib import Path
+from decouple import config,Csv
+import cloudinary.uploader
+import cloudinary.api
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,6 +38,12 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'watch.apps.WatchConfig',
+    'bootstrap4',
+    'cloudinary',
+    'cloudinary_storage',
+    'crispy_forms',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -105,7 +118,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
@@ -117,7 +130,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- nyumbani/urls.py
+++ nyumbani/urls.py
@@ -1,4 +1,4 @@
-"""nyumbani URL Configuration
+"""awardske URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -14,8 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+# from django.conf.urls import include,url
+from django.contrib.auth import views 
+from django_registration.backends.one_step.views import RegistrationView
+
 
 urlpatterns = [
+    
     path('admin/', admin.site.urls),
+    path('',include('watch.urls')),
+    # path('accounts/register/', RegistrationView.as_view(success_url='/create_profile'),name='django_registration_register'),
+    # path('accounts/', include('django_registration.backends.one_step.urls')),
+    # path('accounts/', include('django.contrib.auth.urls')),
+    # path('logout/', views.logout_then_login, name='logout'),
+
 ]
