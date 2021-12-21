b5f91eea2f92fb59fa87e7ebc0edab23fcc7d25b
devseme <ian.ochenge@student.moringaschool.com>
2021-12-21 21:40:46 +0300

    updated index template endblocks

--- watch/models.py
+++ watch/models.py
@@ -1,3 +1,68 @@
 from django.db import models
+from django.db import models
+from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Location(models.Model):
+    name = models.CharField(max_length=30)
+    created_at = models.DateTimeField(auto_now_add=True,null=True)
+    updated_at = models.DateTimeField(auto_now=True,null=True)
+
+    # save location
+    def save_location(self):
+        self.save()
+
+    def _str_(self):
+        return self.name
+# NeighbourHood Model
+class NeighbourHood(models.Model):
+    name = models.CharField(max_length=50)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    occupants_count = models.IntegerField(default=0)
+    admin = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def create_neigborhood(self):
+        self.save()
+
+    @classmethod
+    def delete_neighbourhood(cls, id):
+        cls.objects.filter(id=id).delete()
+
+    @classmethod
+    def update_neighbourhood(cls, id):
+        cls.objects.filter(id=id).update()
+
+    @classmethod
+    def search_by_name(cls, search_term):
+        hood = cls.objects.filter(name__icontains=search_term)
+        return hood
+
+    # find neighbourhood by id
+    @classmethod
+    def find_neigborhood(cls, id):
+        hood = cls.objects.get(id=id)
+        return hood
+
+    def _str_(self):
+        return self.name
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_photo = CloudinaryField('image')
+    bio = models.CharField(max_length=500, blank=True, null=True)
+    contact = models.CharField(max_length=50, blank=True, null=True)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    neighbourhood = models.ForeignKey(NeighbourHood, on_delete=models.CASCADE)
+
+
+    def save_profile(self):
+        self.save() 
+
+
+    def delete_profile(self):
+        self.delete()
+
+    def _str_(self):
+        return self.user.username
