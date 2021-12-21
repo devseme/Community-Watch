a434d1fbb7b1fec40797c3a1f3dcc4c7906ac204
devseme <ian.ochenge@student.moringaschool.com>
2021-12-21 20:43:32 +0300

    updated project urls

--- watch/urls.py
+++ watch/urls.py
@@ -4,11 +4,12 @@ from . import views
 urlpatterns=[
     # url('^$',views.welcome,name = 'welcome'),
     path('',views.index,name= 'index'),
-    # path('profile/', views.profile, name='profile'),
-    # path('accounts/profile/', views.index,name='profile'),
+
+    path('profile/', views.profile, name='profile'),
+    path('accounts/profile/', views.index,name='profile'),
     # path('upload/project/', views.upload, name = "upload"),
-    # path('update_profile/<int:id>',views.update_profile, name='update_profile'),
-    # path('create_profile/',views.create_profile,name = 'create_profile'),
+    path('update_profile/<int:id>',views.update_profile, name='update_profile'),
+    path('create_profile/',views.create_profile,name = 'create_profile'),
     # path('search/',views.search_project, name='search.post'),
     # path("project/<int:project_id>/", views.project_details, name="project_details"),
     # path('rate/<int:id>/',views.rate_project, name='rate.project'),
