63a10ec4b7b4bded3decbf61eb74fd08519e87fe
devseme <ian.ochenge@student.moringaschool.com>
2022-01-08 11:44:40 +0300

    came up with forms for businesses and posts

--- watch/forms.py
+++ watch/forms.py
@@ -1,9 +1,6 @@
 from django import forms
 from django.db.models import fields
-from watch.models import Profile
-
-
-
+from watch.models import *
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
@@ -11,4 +8,18 @@ class ProfileForm(forms.ModelForm):
 class EditProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ('profile_photo','bio','contact')          
+        fields = ('profile_photo','bio','contact')     
+class CreateCommunityForm(forms.ModelForm):
+    class Meta:
+        model=NeighbourHood
+        fields =  ['community_image','community_description','name','location'] 
+class BusinessForm(forms.ModelForm):
+    class Meta:
+        model=Business
+        fields =  ['name','email','description','location','neighborhood']   
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields =['title','image','content','location','neighborhood']       
+
