fe495480431b484f3c9749243e7012bcd3074fde
devseme <ian.ochenge@student.moringaschool.com>
2022-01-08 11:46:05 +0300

    registered my classes

--- watch/admin.py
+++ watch/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
-from .models import Profile,Location,NeighbourHood
+from .models import *
+# NeighbourHood
 
 
 admin.site.register(Profile)
 admin.site.register(Location)
 admin.site.register(NeighbourHood)
+admin.site.register(Business)
